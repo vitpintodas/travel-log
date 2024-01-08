6668a7ae4d602648ba61862c9429d6b8ef8edad1
Julie Achard <julie.achard@heig-vd.ch>
2024-01-08 11:53:34 +0100

    essai1

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -18,6 +18,12 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import ExploreContainer from "@/components/ExploreContainer.vue";
 </script>
