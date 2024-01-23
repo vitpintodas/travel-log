564e9cd4747445206a2b6882f588cbd0c9cb4783
Vitor Da Silva <vitor.pintodasilva@heig-vd.ch>
2024-01-23 11:05:32 +0100

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router';
 
@@ -23,7 +24,9 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+const pinia = createPinia();
 const app = createApp(App)
+  .use(pinia)
   .use(IonicVue)
   .use(router);
   
